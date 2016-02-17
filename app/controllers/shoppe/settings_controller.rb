779d53660a9a8304664b1df310cfe8ed10f20b3c
andreavecy <avecino@accendo.com.co>
2016-02-17 15:04:11 -0500

    Modify demo mode

--- app/controllers/shoppe/settings_controller.rb
+++ app/controllers/shoppe/settings_controller.rb
@@ -1,14 +1,20 @@
 module Shoppe
   class SettingsController < ApplicationController
+
     before_filter { @active_nav = :settings }
-
+    #IF you've registered your admin with a different name, REMEMBER to replace it
+    #It kinda works with everything identifiable on current_user, like email_address
     def update
-      if Shoppe.settings.demo_mode?
-        fail Shoppe::Error, t('shoppe.settings.demo_mode_error')
+      if current_user.full_name == "Default Admin"
+        Shoppe::Setting.update_from_hash(params[:settings].permit!)
+        redirect_to :settings, :notice => t('shoppe.settings.update_notice')
+      else
+        if Shoppe.settings.demo_mode?
+          raise Shoppe::Error, t('shoppe.settings.demo_mode_error')
+        end
+        Shoppe::Setting.update_from_hash(params[:settings].permit!)
+        redirect_to :settings, :notice => t('shoppe.settings.update_notice')
       end
-
-      Shoppe::Setting.update_from_hash(params[:settings].permit!)
-      redirect_to :settings, notice: t('shoppe.settings.update_notice')
     end
   end
 end
